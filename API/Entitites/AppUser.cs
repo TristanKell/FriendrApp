8c8e8fea05a7b7dc10a2cb50d4f5e9660fa40966
tristankllr <tristan.keller@konzeptwerkgmbh.de>
2026-01-23 00:07:54 +0100

    Api Controller Skelett

---
+++ API/Entitites/AppUser.cs
@@ -0,0 +1,11 @@
+namespace API.Entitites;
+
+public class AppUser
+{
+    public String Id { get; set; } = Guid.NewGuid().ToString();
+
+    public required string DisplayName { get; set; }
+    public required string Email { get; set; }
+    
+    
+}
